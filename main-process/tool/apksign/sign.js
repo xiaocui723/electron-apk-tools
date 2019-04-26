386c99eb2385d7091a2fcb1537cb89312008e41a
cuiwenjian <wenjian.cui@rayjoy.com>
2019-04-26 17:43:00 +0800

    签名前增加对齐，签名后检查对齐。

--- main-process/tool/apksign/sign.js
+++ main-process/tool/apksign/sign.js
@@ -1,43 +1,113 @@
-const { app, ipcMain } = require('electron')
+const { app, ipcMain, shell } = require("electron");
 const { spawn } = require('child_process');
 
 ipcMain.on('exec-sign', (event, param) => {
   console.log(param);
+  zipalign(param, event);
+  // event.sender.send('execed-sign', '')
+})
 
-  var toolsDir = app.getAppPath() + '/tools';
-  var apksigner = toolsDir + '/apksigner';
-  
+function zipalign(param, event) {
+  if (!param.signTypeV2) {
+    return;
+  }
+
+  var toolsDir = app.getAppPath() + "/tools";
+  var zipalign = toolsDir + "/zipalign";
+  var apkDir = param.apkDir;
+  var apkIndex = apkDir.indexOf(".apk");
+  var zipApkDir = apkDir.slice(0, apkIndex) + "_zip" + apkDir.slice(apkIndex, apkDir.length);
+
+  var args = ["4", `${param.apkDir}`, `${zipApkDir}`];
+
+  var ret = spawn(zipalign, args);
+  ret.stdout.on("data", function(data) {
+    event.sender.send("execed-sign", data);
+  });
+
+  ret.stderr.on("data", function(data) {
+    event.sender.send("execed-sign", data);
+  });
+
+  ret.on("close", function(code) {
+    event.sender.send(
+      "execed-sign",
+      "child process zipalign exited with code " + code +
+      "\n" +
+      "\n=============================\n\n"
+    );
+    if (code == 0) {
+      param.apkDir = zipApkDir;
+      sign(param, event);
+    }
+  });
+}
+
+function sign(param, event) {
+  var toolsDir = app.getAppPath() + "/tools";
+  var apksigner = toolsDir + "/apksigner";
+
   var args = [
-    'sign',
-    '-v',
-    '--ks',
+    "sign",
+    "--ks",
     `${param.keystoreDir}`,
-    '--v1-signing-enabled',
+    "--v1-signing-enabled",
     `${param.signTypeV1}`,
-    '--v2-signing-enabled',
+    "--v2-signing-enabled",
     `${param.signTypeV2}`,
-    '--ks-key-alias',
+    "--ks-key-alias",
     `${param.alias}`,
-    '--ks-pass',
+    "--ks-pass",
     `pass:${param.aliasPass}`,
-    '--key-pass',
+    "--key-pass",
     `pass:${param.keyPass}`,
-    '--out',
+    "--out",
     `${param.saveDir}`,
-    `${param.apkDir}`,
+    `${param.apkDir}`
   ];
   console.log(args);
   var ret = spawn(apksigner, args);
-  ret.stdout.on('data', function (data) {
-    event.sender.send('execed-sign', data)
+  ret.stdout.on("data", function(data) {
+    event.sender.send("execed-sign", data);
   });
 
-  ret.stderr.on('data', function (data) {
-    event.sender.send('execed-sign', data)
+  ret.stderr.on("data", function(data) {
+    event.sender.send("execed-sign", data);
   });
 
-  ret.on('close', function (code) {
-    event.sender.send('execed-sign', 'child process exited with code ' + code)
-  })
-  // event.sender.send('execed-sign', '')
-})
+  ret.on("close", function(code) {
+    event.sender.send(
+      "execed-sign",
+      "child process sign exited with code " +
+        code +
+        "\n" +
+        "\n=============================\n\n"
+    );
+    if (code == 0) {
+      checkZipalign(param, event);
+    }
+  });
+}
+
+function checkZipalign(param, event) {
+  var toolsDir = app.getAppPath() + "/tools";
+  var zipalign = toolsDir + "/zipalign";
+  var args = ["-c", "4", `${param.saveDir}`];
+
+  var ret = spawn(zipalign, args);
+  ret.stdout.on("data", function(data) {
+    event.sender.send("execed-sign", data);
+  });
+
+  ret.stderr.on("data", function(data) {
+    event.sender.send("execed-sign", data);
+  });
+
+  ret.on("close", function(code) {
+    event.sender.send(
+      "execed-sign",
+      "child process checkZipalign exited with code " + code
+    );
+    shell.showItemInFolder(param.saveDir);
+  });
+}
